ff30426fcf4d387c4d47d3e2fd449b68a4b298ff
Roberto Carlos Hernandez <carlos.238023@gmail.com>
2020-10-25 18:29:17 -0600

    inicio js

---
+++ valores.js
@@ -0,0 +1,21 @@
+40  //valor de tipo numerico
+
+"Diego De Granda"//para guardar palabras
+
+
+// valores de tipo booleanos
+true  // verdadero
+false // falso
+
+
+//valores vacios o reservados para posteriormente poner un valor
+null
+undefined  //tratar de evitarlo
+
+//todos estos valores eran primitivos-----------
+
+
+//no primitivos objetos
+[1,2,3]
+
+{nombre:"Diego"}  //valor de tipo objeto
---
+++ variables.js
@@ -0,0 +1,27 @@
+//que es una variable, como guardar un valor en memoria
+
+var nombre = "Oscar";   //var es una palabra reservada para poder guardar una variable a travez de la palabra nombre
+
+
+//2 estados de una variable
+
+//  declarar
+ var edad;
+edad
+//  inicializar
+edad = 30;
+
+edad
+
+//declarando e inicializando una variable
+var elementos = ["Computadora","Celular"];
+
+var persona = {
+    nombre:"Diego",
+    edad:30
+}
+
+persona;
+
+
+
